6af4265acd19813f4d9b72d8e3e94ca29bb8d78a
michaldudzisz <49537887+michaldudzisz@users.noreply.github.com>
2020-10-29 11:34:40 +0100

    Bruteforce v0.1

---
+++ bruteforce.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <iostream>
+
+char capitalise(char c) {
+    if (c >= 'a' && c <= 'z')
+        c = c + ('A' - 'a');
+    return c;
+}
+
+bool containLowercaseLetter(std::string s) {
+    for (char c : s) {
+        if (c >= 'a' && c <= 'b')
+            return true;
+    }
+    return false;
+}
+
+int main(int argc, char *argv[]) {
+    // argumenty wywolania:
+    std::string a, b;
+    a = std::string(argv[1]);
+    b = std::string(argv[2]);
+
+    // jesli b zawiera male litery
+    if (containLowercaseLetter(b)) {
+        std::cout << "false" << std::endl;
+        return 0;
+    }
+
+    std::string::iterator aIt = a.begin();
+    std::string::iterator bIt = b.begin();
+    while (aIt != a.end() && bIt != b.end()) {
+        // capitalise(*aIt), zeby moc porownac dowolna *aIt z na pewno wielka *bIt
+        if (capitalise(*aIt) == *bIt)
+            bIt++;
+
+        aIt++;
+    }
+
+    if (bIt == b.end())
+        std::cout << "true" << std::endl;
+    else 
+        std::cout << "false" << std::endl;
+    
+
+    return 0;
+}
+

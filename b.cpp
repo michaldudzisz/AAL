2eb1e917475b868f9ce070ec18b98f89475bdcf2
Mike <michalkruk199@gmail.com>
2020-12-06 13:41:41 +0100

    dziala nowy algorytm

--- b.cpp
+++ b.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <cassert>
+#include <algorithm>
 
 using namespace std;
 
 bool bf(string, string);
 bool solve3(string, string);
+bool solve_n2(string, string);
+
 void run_tests(bool(*)(string, string));
 
-int main(/*int argc, char** argv*/) {
-    run_tests(solve3);
-    run_tests(bf);
+int main(int argc, char** argv) {
+  run_tests(solve_n2);
+  run_tests(bf);
 }
 
 void run_tests(bool(*f)(string, string)) {
@@ -32,6 +35,30 @@ void run_tests(bool(*f)(string, string)) {
     assert(!f("ada", "ADB"));
     assert(!f("", "a"));
     assert(!f("andej", "andejduda"));
+    assert(f("bB", "B"));
+    assert(!f("abcBx", "abcBX"));
+}
+
+bool solve_n2(string a, string b) {
+  bool b_contains_lower = false;
+  for (int i = 0; i < b.length(); ++i) if (islower(b[i])) {b_contains_lower = true; break;}
+
+  int prev = 0;
+  for (int i = 0; i < b.length(); ++i) {
+    int pos = a.substr(prev).find(b[i]); 
+    if (pos == string::npos && b_contains_lower) return false; 
+    if (pos == string::npos && isupper(b[i])) pos = a.substr(prev).find(tolower(b[i]));
+    if (pos == string::npos) return false;
+    pos += prev;
+
+    for (int j = prev; j < pos; ++j) if (b_contains_lower || isupper(a[j])) return false;
+    prev = pos + 1;
+  }
+
+  for (int i = prev; i < a.length(); ++i) 
+    if (b_contains_lower || isupper(a[i])) return false;
+
+  return true;
 }
 
 bool solve3(string a, string b) {
